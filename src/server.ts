c0bfb6912e021aec518d1c3c9662b3147275bb82
Margherita Ravanello <63791162+RavaMarghe@users.noreply.github.com>
2022-08-27 11:46:40 +0200

    Configure automatic build and restarts

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import "express-async-errors";
 const app = express();
 
 app.get("/", (request, response) => {
-    response.send("Up and running!")
-})
+    response.send("This is the Space Facts API!");
+});
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}` )
-})
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
